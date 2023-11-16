fbee808b006e3ddb3f4f58d6a0921d14ff8e38f0
Mike Flood <flood.mike@gmail.com>
2023-11-15 20:52:23 -0600

    feat: update which key

--- config/nvim/lua/mike/which-key.lua
+++ config/nvim/lua/mike/which-key.lua
@@ -2,7 +2,15 @@ local wk = require 'which-key'
 
 local function init()
     wk.register({
-        ['<leader>b'] = { name = 'Debug', _ = 'which_key_ignore' }
+        ['<leader>b'] = { name = 'Debug', _ = 'which_key_ignore' },
+        ['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = 'Document', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = 'Git', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Harpoon', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = 'Rename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = 'Search', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = 'Terminal', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = 'Workspace', _ = 'which_key_ignore' },
     })
 end
 
