493c2581e76183e9c26c3c4a3bcc4036b86cb6f0
Mike Flood <flood.mike@gmail.com>
2023-11-15 20:18:39 -0600

    feat: more dashboard work

--- config/nvim/lua/mike/dashboard.lua
+++ config/nvim/lua/mike/dashboard.lua
@@ -1,3 +1,5 @@
+local dashboard = require 'dashboard'
+
 function init()
     vim.g.dashboard_default_executive = 'telescope'
 
@@ -15,6 +17,30 @@ function init()
         [[                              ]],
         [[  There is no place like ~/   ]],
     }
+
+    dashboard.setup({
+        theme = 'doom',
+        hide = {
+            statusline = false,
+            tabline,
+        },
+        config = {
+            center = {
+                { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+                { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+                { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+                { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
+                { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
+                {
+                    action = 'lua require("persistence").load()',
+                    desc = " Restore Session",
+                    icon = " ",
+                    key = "s"
+                },
+                { action = "qa", desc = " Quit", icon = " ", key = "q" },
+            },
+        },
+    })
 end
 
 return {
