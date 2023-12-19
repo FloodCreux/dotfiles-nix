1ae15868a9b09b35fe8b8193a33417076778149e
Mike Flood <flood.mike@gmail.com>
2023-12-18 19:52:40 -0600

    cleanup and rearrange nvim config

--- config/nvim/lua/mike/init.lua
+++ config/nvim/lua/mike/init.lua
@@ -1,7 +1,6 @@
 local function init()
 	require("mike.vim").init()
 	require("mike.lazy").init()
-	require("mike.lazy").install()
 	require("mike.cmp").init()
 	require("mike.git").init()
 end
--- config/nvim/lua/mike/telescope.lua
+++ config/nvim/lua/mike/telescope.lua
@@ -1,61 +1,61 @@
-
 local function init()
-    local telescope = require 'telescope'
+	local telescope = require("telescope")
+	local builtin = require("telescope.builtin")
 
-    telescope.setup {
-        defaults = {
-            file_ignore_patterns = {
-                'node_modules/.*',
-                '%.pem',
-                '.git/.*',
-            },
-            mappings = {
-                i = {
-                    ['<C-u>'] = false,
-                    ['<C-d>'] = false,
-                },
-            },
-        },
-        extensions = {
-            file_browser = {
-                hijack_netrw = true,
-                mappings = {
-                    ['i'] = {},
-                    ['n'] = {},
-                },
-            },
-        },
-        find_files = {
-            hidden = true,
-        },
-    }
+	telescope.setup({
+		defaults = {
+			file_ignore_patterns = {
+				"node_modules/.*",
+				"%.pem",
+				".git/.*",
+			},
+			mappings = {
+				i = {
+					["<C-u>"] = false,
+					["<C-d>"] = false,
+				},
+			},
+		},
+		extensions = {
+			file_browser = {
+				hijack_netrw = true,
+				mappings = {
+					["i"] = {},
+					["n"] = {},
+				},
+			},
+		},
+		find_files = {
+			hidden = true,
+		},
+	})
 
-    telescope.load_extension('fzf')
-    telescope.load_extension('file_browser')
-    telescope.load_extension('metals')
+	telescope.load_extension("fzf")
+	telescope.load_extension("file_browser")
+	telescope.load_extension("metals")
 
+	vim.keymap.set("n", "<leader>ff", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+	vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+	vim.keymap.set("n", "<leader>/", function()
+		-- You can pass additional configuration to telescope to change theme, layout, etc.
+		builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+			winblend = 10,
+			previewer = false,
+		}))
+	end, { desc = "[/] Fuzzily search in current buffer" })
 
-    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-    vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-        })
-    end, { desc = '[/] Fuzzily search in current buffer' })
-
-    vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    vim.keymap.set('n', '<leader>sf', function() require('telescope.builtin').find_files({ hidden = true }) end,
-        { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+	vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search Git Files" })
+	vim.keymap.set("n", "<leader>sf", function()
+		builtin.find_files({ hidden = true })
+	end, { desc = "Search Files" })
+	vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
+	vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Search current Word" })
+	vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search by Grep" })
+	vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
+	vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Search Resume" })
+	vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, { desc = "Goto Definition" })
 end
 
 return {
-    init = init
+	init = init,
 }
