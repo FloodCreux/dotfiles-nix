eb210794b3b040474e580635fa9d633ec9e47298
Mike Flood <flood.mike@gmail.com>
2024-12-04 11:35:09 -0600

    remove some things

--- modules/default.nix
+++ modules/default.nix
@@ -1,43 +1,4 @@
 { inputs }:
-let
-  home-manager = import ./home-manager;
-  metals = import ./metals { inherit inputs; };
-in
 {
-  mkDarwin =
-    {
-      system,
-      pkgs,
-      username,
-    }:
-    inputs.darwin.lib.darwinSystem {
-      modules = [
-        (import ./darwin {
-          inherit pkgs;
-          inherit username;
-        })
-
-        inputs.home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${username} =
-              { ... }:
-              {
-                imports = [
-                  (home-manager {
-                    inherit pkgs;
-                    inherit username;
-                  })
-                ];
-              };
-          };
-        }
-      ];
-
-      system = system;
-    };
-
-  metalsOverlay = metals;
+  metalsOverlay = import ./metals { inherit inputs; };
 }
