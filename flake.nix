7f4d336feb511d97cf172559b1065a8fb10cedc0
Mike Flood <flood.mike@gmail.com>
2025-08-06 07:56:50 -0500

    refactor flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   description = "Development packages for Mike Flood";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-
     zig.url = "github:mitchellh/zig-overlay";
     ghostty.url = "github:ghostty-org/ghostty";
 
-    # rust overlay
     fenix = {
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -34,11 +36,7 @@
   };
 
   outputs =
-    inputs@{
-      nixpkgs,
-      darwin,
-      ...
-    }:
+    inputs@{ nixpkgs, darwin, ... }:
     let
       pkgs-config = {
         allowUnfree = true;
@@ -52,61 +50,37 @@
         ];
       };
 
+      mkDarwinSystem =
+        {
+          username,
+          system ? "aarch64-darwin",
+        }:
+        let
+          # Import lib only for work configuration
+          lib = if username == "chmc-h022fl97xj" then import ./lib { inherit pkgs inputs; } else null;
+
+          overlays = import ./lib/overlays.nix (
+            { inherit inputs system username; } // (if lib != null then { inherit lib; } else { })
+          );
+
+          pkgs = import nixpkgs {
+            inherit system overlays;
+            config = pkgs-config;
+          };
+        in
+        pkgs.mkDarwin {
+          inherit
+            darwin
+            system
+            pkgs
+            username
+            ;
+        };
     in
     {
       darwinConfigurations = {
-        default =
-          let
-            system = "aarch64-darwin";
-            username = "mike";
-
-            overlays = import ./lib/overlays.nix { inherit inputs system username; };
-
-            pkgs = import nixpkgs {
-              inherit overlays system;
-
-              config = pkgs-config;
-            };
-          in
-          pkgs.mkDarwin {
-            inherit
-              darwin
-              system
-              pkgs
-              username
-              ;
-          };
-
-        work =
-          let
-            system = "aarch64-darwin";
-            username = "chmc-h022fl97xj";
-
-            lib = import ./lib { inherit pkgs inputs; };
-
-            overlays = import ./lib/overlays.nix {
-              inherit
-                lib
-                inputs
-                system
-                username
-                ;
-            };
-
-            pkgs = import nixpkgs {
-              inherit system overlays;
-
-              config = pkgs-config;
-            };
-          in
-          pkgs.mkDarwin {
-            inherit
-              darwin
-              system
-              pkgs
-              username
-              ;
-          };
+        default = mkDarwinSystem { username = "mike"; };
+        work = mkDarwinSystem { username = "chmc-h022fl97xj"; };
       };
     };
 }
