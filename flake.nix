685d3ec33cd06df35983a24690eefb2cccfb2335
Mike Flood <flood.mike@gmail.com>
2025-10-07 15:01:03 -0500

    flake-parts

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
@@ -25,51 +26,56 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, darwin, ... }:
-    let
-      pkgs-config = {
-        allowUnfree = true;
-        allowUnsupportedSystem = true;
-        permittedInsecurePackages = [
-          "dotnet-core-combined"
-          "dotnet-sdk-6.0.428"
-          "dotnet-sdk-wrapped-6.0.428"
-          "dotnet-runtime-6.0.36"
-          "dotnet-runtime-wrapped-6.0.36"
-        ];
-      };
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" ];
+      perSystem = { system, pkgs, ... }: { };
 
-      mkDarwinSystem =
-        {
-          username,
-          system ? "aarch64-darwin",
-        }:
-        let
-          # Import lib only for work configuration
-          lib = if username == "chmc-h022fl97xj" then import ./lib { inherit pkgs inputs; } else null;
+      flake = {
+        darwinConfigurations =
+          let
+            darwin = inputs.darwin;
 
-          overlays = import ./lib/overlays.nix (
-            { inherit inputs system username; } // (if lib != null then { inherit lib; } else { })
-          );
+            pkgs-config = {
+              allowUnfree = true;
+              allowUnsupportedSystem = true;
+              permittedInsecurePackages = [
+                "dotnet-core-combined"
+                "dotnet-sdk-6.0.428"
+                "dotnet-sdk-wrapped-6.0.428"
+                "dotnet-runtime-6.0.36"
+                "dotnet-runtime-wrapped-6.0.36"
+              ];
+            };
 
-          pkgs = import nixpkgs {
-            inherit system overlays;
-            config = pkgs-config;
+            mkDarwinSystem =
+              {
+                username,
+                system ? "aarch64-darwin",
+              }:
+              let
+                lib = if username == "chmc-h022fl97xj" then import ./lib { inherit pkgs inputs; } else null;
+                overlays = import ./lib/overlays.nix (
+                  { inherit inputs system username; } // (if lib != null then { inherit lib; } else { })
+                );
+                pkgs = import inputs.nixpkgs {
+                  inherit system overlays;
+                  config = pkgs-config;
+                };
+              in
+              pkgs.mkDarwin {
+                inherit
+                  darwin
+                  system
+                  pkgs
+                  username
+                  ;
+              };
+          in
+          {
+            default = mkDarwinSystem { username = "mike"; };
+            work = mkDarwinSystem { username = "chmc-h022fl97xj"; };
           };
-        in
-        pkgs.mkDarwin {
-          inherit
-            darwin
-            system
-            pkgs
-            username
-            ;
-        };
-    in
-    {
-      darwinConfigurations = {
-        default = mkDarwinSystem { username = "mike"; };
-        work = mkDarwinSystem { username = "chmc-h022fl97xj"; };
       };
     };
 }
