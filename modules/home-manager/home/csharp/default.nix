e8eda039bafa7adfb6e1b6ab1b69615dd14d9d0a
Mike Flood <flood.mike@gmail.com>
2025-10-13 08:50:23 -0500

    add more dotnet

--- modules/home-manager/home/csharp/default.nix
+++ modules/home-manager/home/csharp/default.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    dotnet-sdk
+    (
+      with dotnetCorePackages;
+      combinePackages [
+        sdk_10_0
+        sdk_9_0
+        sdk_8_0
+      ]
+    )
     omnisharp-roslyn
     roslyn
     csharp-ls
