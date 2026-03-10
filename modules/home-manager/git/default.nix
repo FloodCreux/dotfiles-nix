64211ee26109d5744905f1e7e0ea6c53edda7814
Mike Flood <flood.mike@proton.me>
2026-03-10 09:13:27 -0500

    update git options

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -2,5 +2,27 @@
 {
   programs.git = {
     enable = true;
+    settings = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      rerere.enabled = true;
+      diff.algorithm = "histogram";
+      merge.conflictstyle = "zdiff3";
+    };
+    ignores = [
+      ".DS_Store"
+      "*.swp"
+      ".direnv"
+    ];
+  };
+
+  programs.delta = {
+    enable = true; # Better diff viewer
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      side-by-side = true;
+    };
   };
 }
