c10e66e42a4a3615752d03027e6fce2bbcb210ba
Mike Flood <flood.mike@proton.me>
2026-03-10 09:35:18 -0500

    revert go for now

--- modules/darwin/go/default.nix
+++ modules/darwin/go/default.nix
@@ -1,12 +1,4 @@
+{ pkgs, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  options.modules.go.enable = lib.mkEnableOption "Go";
-  config = lib.mkIf config.modules.go.enable {
-    environment.systemPackages = with pkgs; [ go ];
-  };
+  environment.systemPackages = with pkgs; [ go ];
 }
